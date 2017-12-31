f048bb4749db93eef1eec771e01cbbf60729ec98
Jonathan Pearson <jon@jpoffline.com>
2017-12-31 17:06:50 +0000

    created a main2 function

--- src/test.cpp
+++ src/test.cpp
@@ -8,11 +8,11 @@
 
 #include <iostream>
 #include "jvector.h"
+#include "jmatrix.h"
 
 
 
-
-int main() {
+int main2() {
 	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	auto v = JP::GVECTOR<double>();
 	v.push_back(21.3);
@@ -20,11 +20,15 @@ int main() {
 
 	std::vector<std::vector<double>> m1{{1,1,1},{1,1,1}};
 	std::vector<std::vector<double>> m2{{1,1},{1,1}, {1,1}};
-	//auto m3 = JP::MATH::MATRIX::multiply(m1, m1);
-	JP::MATH::MATRIX::print(m1);
-	//JP::MATH::MATRIX::print(m3);
+	auto m3 = JP::MATH::MATRIX::multiply(m1, m2);
+	JP::MATH::MATRIX::print(m3);
 
 
 
+	return 0;
+}
+
+int main() {
+	main2();
 	return 0;
 }
