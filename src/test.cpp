86a40704e468d077c77d1b58ad4d88bdd76e9c62
Jonathan Pearson <jon@jpoffline.com>
2017-12-31 16:49:11 +0000

    stuff

--- src/test.cpp
+++ src/test.cpp
@@ -7,9 +7,24 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
-//tmp
+#include "jvector.h"
+
+
+
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	auto v = JP::GVECTOR<double>();
+	v.push_back(21.3);
+	v.print_size();
+
+	std::vector<std::vector<double>> m1{{1,1,1},{1,1,1}};
+	std::vector<std::vector<double>> m2{{1,1},{1,1}, {1,1}};
+	//auto m3 = JP::MATH::MATRIX::multiply(m1, m1);
+	JP::MATH::MATRIX::print(m1);
+	//JP::MATH::MATRIX::print(m3);
+
+
+
 	return 0;
 }
