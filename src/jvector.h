4a9cce19577ba3f61cfb96c1078d43272e92a732
Jonathan Pearson <jon@jpoffline.com>
2017-12-31 17:06:42 +0000

    removed matrix stuff

--- src/jvector.h
+++ src/jvector.h
@@ -11,55 +11,7 @@
 #include <iostream>
 namespace JP
 {
-namespace MATH
-{
-namespace MATRIX
-{
 
-template<typename T>
-void print(const std::vector<std::vector<T>>& m)
-{
-	std::cout << "n rows: " << m.size() << std::endl;
-	std::cout << "n cols: " << m[0].size() << std::endl;
-	for (auto v : m)
-	{
-		for (auto vv : v)
-		{
-			std::cout << vv << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-template<typename T>
-std::vector<std::vector<T>> multiply(const std::vector<std::vector<T>>& m1,
-		const std::vector<std::vector<T>>& m2)
-{
-
-	auto nr1 = m1.size();
-	auto nc1 = m1[0].size();
-	auto nr2 = m2.size();
-	auto nc2 = m2[0].size();
-
-	auto check1 = nc1 == nr2;
-
-	std::vector<std::vector<T>> result(nr2, std::vector<T>(nc2));
-	for (int i = 0; i < nr2; i++)
-	{
-		for (int j = 0; j < nc2; j++)
-		{
-			for (int k = 0; k < nr2; k++)
-			{
-				result[i][j] += m1[i][k] * m2[k][j];
-			}
-		}
-	}
-	return result;
-}
-
-}
-
-}
 
 template<typename VT>
 class GVECTOR
